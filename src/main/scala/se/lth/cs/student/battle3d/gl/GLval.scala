d00e0b966911361a52c9512e47e10ef03edf0587
Kss0N <ja6764kr-s@student.lu.se>
2022-11-30 11:53:44 +0100

    `feat` added method fromGL

--- src/main/scala/se/lth/cs/student/battle3d/gl/GLval.scala
+++ src/main/scala/se/lth/cs/student/battle3d/gl/GLval.scala
@@ -1,4 +1,4 @@
 package se.lth.cs.student.battle3d.gl
 
-abstract class GLval protected(`val`: Int):
+abstract class GLval protected(val `val`: Int):
     def get : Int = `val`
--- src/main/scala/se/lth/cs/student/battle3d/gl/topology.scala
+++ src/main/scala/se/lth/cs/student/battle3d/gl/topology.scala
@@ -1,8 +1,10 @@
 package se.lth.cs.student.battle3d.gl
 
 import com.jogamp.opengl.GL
+import scala.util.Failure
+import scala.util.Success
 
-enum Topology(val `val`:Int) extends GLval(`val`):
+enum Topology(override val `val`:Int) extends GLval(`val`):
     case POINTS         extends Topology(GL.GL_POINTS)
     case LINES          extends Topology(GL.GL_LINES)
     case LINE_LOOP      extends Topology(GL.GL_LINE_LOOP)
@@ -10,3 +12,11 @@ enum Topology(val `val`:Int) extends GLval(`val`):
     case TRIANGLES      extends Topology(GL.GL_TRIANGLES)
     case TRIANGLE_STRIP extends Topology(GL.GL_TRIANGLE_STRIP)
     case TRIANGLE_FAN   extends Topology(GL.GL_TRIANGLE_FAN)
+
+object Topology:
+
+    /** selects enum value from openGL value*/
+    def fromGL(v: Int): Topology =
+        util.Try{Topology.values.filter(v == _.get)(0)} match
+            case Failure(exception) => throw new java.lang.IllegalArgumentException(s"Value $v can't be used to construct enum Topology")
+            case Success(value) => value
