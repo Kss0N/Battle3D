3263011d04b344485e81b2a5c2b22cc3afaaf535
Kss0N <ja6764kr-s@student.lu.se>
2022-12-02 10:14:06 +0100

    add fromGL & byteSize to AttribType

--- src/main/scala/se/lth/cs/student/battle3d/gl/AttribType.scala
+++ src/main/scala/se/lth/cs/student/battle3d/gl/AttribType.scala
@@ -2,6 +2,9 @@ package se.lth.cs.student.battle3d.gl
 
 
 import com.jogamp.opengl.GL
+import org.w3c.dom.Attr
+import scala.util.Failure
+import scala.util.Success
 
 /** Attribute Type, typically used with `VertexArray.setVertexAttribFormat`
 * @param val OpenGL value
@@ -13,3 +16,19 @@ enum AttribType private (`val` : Int) extends GLval(`val`):
     case UNSIGNED_SHORT extends AttribType(GL.GL_UNSIGNED_SHORT)
     case UNSIGNED_INT   extends AttribType(GL.GL_UNSIGNED_INT)
     case FLOAT          extends AttribType(GL.GL_FLOAT)
+
+object AttribType:
+
+    /** finds the suitable AttribType from an OpenGL value */
+    def fromGL(v: Int) = 
+        util.Try {AttribType.values.filter(e =>  v == e.get)(0)} match
+            case Failure(exception) => throw new IllegalArgumentException(s"Attribute Type can't be of value $v")
+            case Success(value) => value
+    
+    def byteSize(e: AttribType) =
+        e match
+            //An other lucky bit from the KhronosGroup, the component type is equal to the GL_TYPE
+            case AttribType.BYTE | AttribType.UNSIGNED_BYTE => 1
+            case AttribType.SHORT| AttribType.UNSIGNED_SHORT=> 2
+            case AttribType.FLOAT| AttribType.UNSIGNED_INT  => 4        
+    
