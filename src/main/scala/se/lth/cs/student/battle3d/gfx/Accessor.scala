662e773ecb9037635b09f611bdd9d12c4548a89e
Kss0N <ja6764kr-s@student.lu.se>
2022-12-02 10:44:43 +0100

    document Accessor.scala

--- src/main/scala/se/lth/cs/student/battle3d/gfx/Accessor.scala
+++ src/main/scala/se/lth/cs/student/battle3d/gfx/Accessor.scala
@@ -1,8 +1,10 @@
 package se.lth.cs.student.battle3d.gfx
 
 import se.lth.cs.student.battle3d.gl.AttribType
+import se.lth.cs.student.battle3d.gl.Topology
 
 import java.nio.Buffer
+
 
 /** Describes a buffer where data used by the renderer is stored
   * 
@@ -14,15 +16,17 @@ import java.nio.Buffer
   * @param offset       offset into buffer where data is stored (bytes)
   * @param size         of data (in bytes)
   * @param stride       of each data point (in bytes)
-  * @param type         dataType of data
+  * @param type         dataType of data, default: Float
+  * @param mode         (In case of vertices) mode of drawing
   * @param normalized   if data will be normalized, default: no
   */
-final case class Accessor(
+  final case class Accessor(
     val name        : String, 
     val buffer      : Buffer, 
     val offset      : Long, 
     val size        : Long,
     val stride      : Int, 
-    val `type`      : AttribType,
+    val `type`      : AttribType = AttribType.FLOAT,
+    val mode        : Topology = Topology.TRIANGLES,
     val normalized  : Boolean = false
 )
