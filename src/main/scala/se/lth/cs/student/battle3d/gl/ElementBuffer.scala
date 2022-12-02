b2d49622e143b60ab62afe17dbc450871c9d5c00
Kss0N <ja6764kr-s@student.lu.se>
2022-12-02 10:13:39 +0100

    add ElementBuffer constructor

--- src/main/scala/se/lth/cs/student/battle3d/gl/ElementBuffer.scala
+++ src/main/scala/se/lth/cs/student/battle3d/gl/ElementBuffer.scala
@@ -3,5 +3,12 @@ package se.lth.cs.student.battle3d.gl
 import com.jogamp.opengl.GL4
 import se.lth.cs.student.battle3d.gl.Target
 
-final case class ElementBuffer(val ebo: Int)(using gl: GL4) extends GLBuffer(ebo):
+final case class ElementBuffer private(val ebo: Int)(using gl: GL4) extends GLBuffer(ebo):
     override val target: Target = Target.ELEMENT_ARRAY_BUFFER
+
+object ElementBuffer:
+
+    def ElementBuffer()(using gl: GL4): ElementBuffer = 
+        val buffers = Array.fill[Int](1)(0)
+        gl.glCreateBuffers(1, buffers, 0)
+        new ElementBuffer(buffers(0))
