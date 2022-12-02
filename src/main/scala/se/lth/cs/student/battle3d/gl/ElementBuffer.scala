06245812d065fd1c213625c719e119ca5799c72b
Kss0N <ja6764kr-s@student.lu.se>
2022-12-02 12:06:33 +0100

    renamed ElementBuffer.ElementBuffer to apply

--- src/main/scala/se/lth/cs/student/battle3d/gl/ElementBuffer.scala
+++ src/main/scala/se/lth/cs/student/battle3d/gl/ElementBuffer.scala
@@ -8,7 +8,7 @@ final case class ElementBuffer private(val ebo: Int)(using gl: GL4) extends GLBu
 
 object ElementBuffer:
 
-    def ElementBuffer()(using gl: GL4): ElementBuffer = 
+    def apply()(using gl: GL4): ElementBuffer = 
         val buffers = Array.fill[Int](1)(0)
         gl.glCreateBuffers(1, buffers, 0)
         new ElementBuffer(buffers(0))
