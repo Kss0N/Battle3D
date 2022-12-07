66897c0d272584670d183bb32f93d58c83f6f89f
Kss0N <ja6764kr-s@student.lu.se>
2022-12-07 14:57:46 +0100

    `feat` implement Camera

--- src/main/scala/se/lth/cs/student/battle3d/gfx/Camera.scala
+++ src/main/scala/se/lth/cs/student/battle3d/gfx/Camera.scala
@@ -1,9 +1,36 @@
 package se.lth.cs.student.battle3d.gfx
 
+import se.lth.cs.student.battle3d.io.Display
 
-import glm.mat._4.Mat4
-import jglm.Jglm
+import jglm.{
+    Jglm,
+    Mat4,
+    Vec3,
+}
+
 
 
 object Camera:
-    private var matrix: Mat4 = new Mat4()
+    private var _matrix: Mat4 = new Mat4()
+
+    var fov = (45.0f * math.Pi/180).toFloat
+    var nearPlane = 0.1f
+    var  farPlane = 100.0f
+
+    def matrix: Mat4 = Jglm.perspective( 
+        fov,
+        Display.dim(0)/Display.dim(1),  //Aspect Ration width/height
+        nearPlane,                      //clip near
+        farPlane)                       //clip far
+        .mult(_matrix)             
+
+    def move(pos : Vec3): Unit = 
+        _matrix = _matrix.mult(Mat4.translate(pos))
+    def rotateX(angle: Float): Unit = 
+        _matrix = _matrix.mult(Mat4.rotationX(angle))
+    def rotateY(angle: Float): Unit = 
+        _matrix = _matrix.mult(Mat4.rotationY(angle))
+    def rotateZ(angle: Float): Unit = 
+        _matrix = _matrix.mult(Mat4.rotationZ(angle))
+
+
